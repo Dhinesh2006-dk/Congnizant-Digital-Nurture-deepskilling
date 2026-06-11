7b399d8ad6d712d0a56702d5483e12020a97a221
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-06-11 19:05:40 +0530

    Update FactoryMethodTest.java

--- Design_Patterns_Principles_module/FactoryMethodPatternExample/FactoryMethodTest.java
+++ Design_Patterns_Principles_module/FactoryMethodPatternExample/FactoryMethodTest.java
@@ -4,30 +4,19 @@ public class FactoryMethodTest
     {
         DocumentFactory wordFactory =
                 new WordFactory();
-
         Document word =
                 wordFactory.createDocument();
 
         word.open();
-
-
-
         DocumentFactory pdfFactory =
                 new PdfFactory();
-
         Document pdf =
                 pdfFactory.createDocument();
-
         pdf.open();
-
-
-
         DocumentFactory excelFactory =
                 new ExcelFactory();
-
         Document excel =
                 excelFactory.createDocument();
-
         excel.open();
     }
 }
