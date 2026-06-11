3b2978072ea83ac667f7eae93e453a75532c0894
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-06-11 21:40:31 +0530

    Add files via upload

---
+++ Design_Patterns_Principles_module/StrategyPatternExample/StrategyPatternTest.java
@@ -0,0 +1,9 @@
+public class StrategyPatternTest {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext(new CreditCardPayment());
+        context.executePayment(5000);
+
+        context.setStrategy(new PayPalPayment());
+        context.executePayment(3000);
+    }
+}
