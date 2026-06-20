cfdf8058c9fb68a761e4f35c43ddae203b814394
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-06-20 15:25:01 +0530

    Add files via upload

---
+++ JUnit_Spring_Test/Exercise8_ControllerExceptionHandling/src/main/java/UserService.java
@@ -0,0 +1,8 @@
+import java.util.NoSuchElementException;
+import org.springframework.stereotype.Service;
+@Service
+public class UserService {
+    public User getUserById(Long id) {
+        throw new NoSuchElementException();
+    }
+}
