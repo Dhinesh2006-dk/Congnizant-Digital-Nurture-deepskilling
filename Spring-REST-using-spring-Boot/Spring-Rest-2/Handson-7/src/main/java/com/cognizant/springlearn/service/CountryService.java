25c99ba888b20441fac5dd504fc3e928c4d80b31
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-07-02 21:59:22 +0530

    Add files via upload

---
+++ Spring-REST-using-spring-Boot/Spring-Rest-2/Handson-7/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,49 @@
+package com.cognizant.springlearn.service;
+
+import java.util.List;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryService.class);
+
+    @SuppressWarnings("unchecked")
+    public Country getCountry(String code)
+            throws CountryNotFoundException {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext(
+                        "country.xml");
+
+        List<Country> countries =
+                (List<Country>) context.getBean(
+                        "countryList");
+
+        for (Country country : countries) {
+
+            if (country.getCode()
+                    .equalsIgnoreCase(code)) {
+
+                LOGGER.info("END");
+                return country;
+            }
+        }
+
+        LOGGER.error("Country not found");
+
+        throw new CountryNotFoundException(
+                "Country not found");
+    }
+}
