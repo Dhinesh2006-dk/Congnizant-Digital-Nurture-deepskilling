4f3d83de9cd0d0ea4c67da9893a32b844b605316
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-07-05 12:12:20 +0530

    Add files via upload

---
+++ Spring-REST-using-spring-Boot/Spring-Rest-4/Handson-4/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,34 @@
+package com.cognizant.springlearn.controller;
+
+import jakarta.validation.Valid;
+
+import com.cognizant.springlearn.model.Country;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @PostMapping
+    public Country addCountry(
+            @RequestBody @Valid Country country) {
+
+        LOGGER.info("START");
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+}
