e4d4b83e0fc9e0394a82c502a9bef12cf3852229
DHINESHKUMAR S <dhinesh182006@gmail.com>
2026-07-17 21:56:40 +0530

    Add files via upload

---
+++ React-HandsOn/scorecalculatorapp/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CalculateScore
+
+        name="Rahul"
+
+        school="ABC Higher Secondary School"
+
+        total={450}
+
+        goal={5}
+
+      />
+
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ React-HandsOn/scorecalculatorapp/src/Components/CalculateScore.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Stylesheets/mystyle.css";
+function CalculateScore(props) {
+    const average = props.total / props.goal;
+    return (
+        <div className="box">
+            <h2>Student Score Card</h2>
+            <p><strong>Name:</strong> {props.name}</p>
+            <p><strong>School:</strong> {props.school}</p>
+            <p><strong>Total Marks:</strong> {props.total}</p>
+            <p><strong>Number of Subjects:</strong> {props.goal}</p>
+            <h3>Average Score: {average}</h3>
+        </div>
+    );
+}
+export default CalculateScore;
